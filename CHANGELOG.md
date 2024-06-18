198d95d23f28d51bb4912d068a31e27aba2e586f
Anatolii Bolshakov <anatoly1410@gmail.com>
2024-06-18 23:35:41 +0500

    Added draft changelog. Created separate folder for user scripts

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog for current version - 0.1.0
+
+Features:
+
+- TBD
--- install.ps1
+++ install.ps1
@@ -1,22 +1,36 @@
 # This script installs the quick commands
 # f - runs desktop notification and sound. You can use in combination with other commands - to get notification about commands finish
 
+function Get-AbsolutePath() {
+    param (
+        [string]$ParentFolderPath,
+        [string]$RelativePath
+    )
+
+    return Resolve-Path -Path (Join-Path -Path $ParentFolderPath -ChildPath $RelativePath)
+}
+
+$ErrorActionPreference = "Stop"
+
 Write-Output "Installing dependencies"
 pip install -r CompleteNotification/requirements.txt
 
 Write-Output "Updating Path environment variable"
 
-$relativeCommandsFolderPath = ".\aliasesScripts"
+$predefinedAliasScriptsFolderPath = "predefinedAliasScripts"
+$userAliasScriptsFolderPath = "userAliasScripts"
 
-$scriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$parentDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
-$absoluteCommandsFolderPath = Resolve-Path -Path (Join-Path -Path $scriptDirectory -ChildPath $relativeCommandsFolderPath)
+$predefinedAliasScriptsAbsolutePath = Get-AbsolutePath -ParentFolderPath $parentDirectory -RelativePath $predefinedAliasScriptsFolderPath
+$userAliasScriptsAbsolutePath = Get-AbsolutePath -ParentFolderPath $parentDirectory -RelativePath $userAliasScriptsFolderPath
 
-Write-Output "Quick commands folder path: $absolutePath"
+Write-Output "Predefined aliases folder path: $predefinedAliasScriptsAbsolutePath"
+Write-Output "User aliases folder path: $userAliasScriptsAbsolutePath"
 
 Write-Output "Updating system path variable..."
 $currentPath = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
-$updatedPath = $currentPath + ";" + $absoluteCommandsFolderPath
+$updatedPath = $currentPath + ";" + $predefinedAliasScriptsAbsolutePath + ";" + $userAliasScriptsAbsolutePath
 
 [System.Environment]::SetEnvironmentVariable("Path", $updatedPath, [System.EnvironmentVariableTarget]::Machine)
 
